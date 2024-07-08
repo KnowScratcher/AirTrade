19cddcd2d761940ee674615a9345a755e69a18cf
KnowScratcher <yianlee2008@gmail.com>
2024-07-08 20:45:05 +0800

    basic inventory

--- src/com/ks/airtrade/commands/Shop.java
+++ src/com/ks/airtrade/commands/Shop.java
@@ -1,6 +1,8 @@
 package com.ks.airtrade.commands;
 
 import com.ks.airtrade.constants.ShopName;
+import com.ks.airtrade.constants.Theme;
+import com.ks.airtrade.file.Bank;
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.Material;
@@ -18,6 +20,14 @@ public class Shop implements CommandExecutor {
     public boolean onCommand(CommandSender sender, Command cmd, String s, String[] args) {
 
         if (sender instanceof Player player) {
+
+            String name = player.getName();
+            int balance = Bank.bank.getInt(name,-1); // deal with new player
+            if (balance == -1) {
+                Bank.bank.set(name,0);
+                Bank.save();
+            }
+
             Inventory shop = Bukkit.createInventory(player,36, ShopName.MAIN);
 
             ItemStack player_head = new ItemStack(Material.PLAYER_HEAD);
@@ -25,9 +35,14 @@ public class Shop implements CommandExecutor {
             assert player_head_meta != null;
             player_head_meta.setOwnerProfile(player.getPlayerProfile());
             player_head_meta.setDisplayName(ChatColor.YELLOW + player.getName());
+            player_head.setItemMeta(player_head_meta);
 
+            ItemStack[] items = {player_head};
+            shop.setContents(items);
+            player.openInventory(shop);
+        }else {
+            sender.sendMessage(Theme.mainTheme("WARNING: You cannot execute this command here."));
         }
-
         return true;
     }
 }
